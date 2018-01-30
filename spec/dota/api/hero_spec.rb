d0c5800c9790f10b633376636f01b640035dbb88
Imir Kiyamov <makketagg@gmail.com>
2018-01-30 21:16:08 +0300

    added heroes type

--- spec/dota/api/hero_spec.rb
+++ spec/dota/api/hero_spec.rb
@@ -1,27 +1,31 @@
 RSpec.describe Dota::API::Hero do
   let(:hero) { described_class.new(69) }
 
-  specify ".all" do
+  specify '.all' do
     heroes = described_class.all
     expect(heroes.first).to be_a described_class
-    expect(heroes.count).to eq 113
+    expect(heroes.count).to eq 115
   end
 
-  specify ".find" do
+  specify '.find' do
     expect(described_class.find(69)).to be_a Dota::API::Hero
     expect(described_class.find(nil)).to be_a Dota::API::MissingHero
-    expect(described_class.find("abc")).to be_a Dota::API::MissingHero
+    expect(described_class.find('abc')).to be_a Dota::API::MissingHero
   end
 
-  specify "#id" do
+  specify '#id' do
     expect(hero.id).to eq 69
   end
 
-  specify "#name" do
-    expect(hero.name).to eq "Doom"
+  specify '#name' do
+    expect(hero.name).to eq 'Doom'
   end
 
-  specify "#image_url" do
-    expect(hero.image_url).to eq "http://cdn.dota2.com/apps/dota2/images/heroes/doom_bringer_full.png"
+  specify '#type' do
+    expect(hero.type).to eq 'strength'
+  end
+
+  specify '#image_url' do
+    expect(hero.image_url).to eq 'http://cdn.dota2.com/apps/dota2/images/heroes/doom_bringer_full.png'
   end
 end
