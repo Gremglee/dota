41ad69d2cfa8f802f8ce92c6082dbcf3e834e6d7
Eugene Korneev <ekorneeff@gmail.com>
2019-08-26 12:13:26 +0300

    Update ability.rb

--- lib/dota/api/ability.rb
+++ lib/dota/api/ability.rb
@@ -13,11 +13,11 @@ module Dota
         ab = mapping[sid]
         ab = {'name' => nil, 'human_name' => nil, 'manacost' => nil, 'cooldown' => nil, 'behavior' => nil} if ab.nil?
         
-        @internal_name = mapping[sid]['name'] || "unknown_ability_#{sid}"
-        @name = mapping[sid]['human_name'] || @internal_name
-        @manacost = mapping[sid]['manacost']
-        @cooldown = mapping[sid]['cooldown']
-        @behavior = mapping[sid]['behavior']
+        @internal_name = ab['name'] || "unknown_ability_#{sid}"
+        @name = ab['human_name'] || @internal_name
+        @manacost = ab['manacost']
+        @cooldown = ab['cooldown']
+        @behavior = ab['behavior']
       end
 
       def image_url(type = :lg)
